affe64aca56fde4b223677fc021b03758d674689
alienspaces <alienspaces@gmail.com>
2021-07-25 15:43:21 +1000

    Add character and action interface

---
+++ server/src/interfaces/action.interface.ts
@@ -0,0 +1,5 @@
+export interface Action {
+    id: string;
+    created_at: string;
+    updated_at: string;
+}
---
+++ server/src/interfaces/character.interface.ts
@@ -0,0 +1,10 @@
+export interface Character {
+    id: string;
+    location_id: string;
+    name: string;
+    strength: number;
+    dexterity: number;
+    intelligence: number;
+    coin: number;
+    experience: number;
+}
