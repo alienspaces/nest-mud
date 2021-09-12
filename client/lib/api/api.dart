3b6124f3942ecfa94b108ca7d7d66410f1d1e524
alienspaces <alienspaces@gmail.com>
2021-09-12 19:28:19 +1000

    Fleshing out backend API client

--- client/lib/api/api.dart
+++ client/lib/api/api.dart
@@ -2,14 +2,58 @@ import 'package:http/http.dart' as http;
 import 'package:http/retry.dart';
 
 class API {
+  final String hostname;
+
+  API({required this.hostname});
+
   Future<String> test() async {
     final client = RetryClient(http.Client());
     String response;
     try {
-      response = await client.read(Uri(host: 'example.org', scheme: 'http'));
+      response = await client.read(Uri(scheme: 'https', host: this.hostname));
     } finally {
       client.close();
     }
     return response;
+  }
+
+  // TODO: Actual implementation
+  Future<String> getDungeons() async {
+    final client = RetryClient(http.Client());
+    String response;
+    try {
+      response = await client.read(Uri(scheme: 'https', host: '${this.hostname}/api/dungeons'));
+    } finally {
+      client.close();
+    }
+    return response;
+  }
+
+  // TODO: Actual implementation
+  Future<String> createCharacter(String dungeonID) async {
+    final client = RetryClient(http.Client());
+    http.Response response;
+    try {
+      response = await client.post(
+          Uri(scheme: 'https', host: '${this.hostname}/api/dungeons/${dungeonID}/characters'),
+          body: {});
+    } finally {
+      client.close();
+    }
+    return response.body;
+  }
+
+  // TODO: Actual implementation
+  Future<String> createAction(String dungeonID) async {
+    final client = RetryClient(http.Client());
+    http.Response response;
+    try {
+      response = await client.post(
+          Uri(scheme: 'https', host: '${this.hostname}/api/dungeons/${dungeonID}/actions'),
+          body: {});
+    } finally {
+      client.close();
+    }
+    return response.body;
   }
 }
--- client/test/api/api_test.dart
+++ client/test/api/api_test.dart
@@ -5,7 +5,7 @@ import 'package:client/api/api.dart';
 
 void main() {
   test('API should ', () async {
-    final api = API();
+    final api = API(hostname: 'example.com');
     expect(api, isNotNull);
     expect(await api.test(), isNotEmpty);
   });
