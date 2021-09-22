45fbb7529f399d7b81334e83a0db22e4fd7838aa
alienspaces <alienspaces@gmail.com>
2021-09-22 19:45:21 +1000

    Add additional character attributes

--- client/lib/repository/character/character_record.dart
+++ client/lib/repository/character/character_record.dart
@@ -2,11 +2,16 @@
 part of 'character_repository.dart';
 
 class CharacterRecord extends Equatable {
-  final int? id;
+  final String? id;
   final String name;
   final int strength;
   final int dexterity;
   final int intelligence;
+  final int? health;
+  final int? fatigue;
+  final int? coins;
+  final int? experience_points;
+  final int? attribute_points;
 
   CharacterRecord({
     this.id,
@@ -14,6 +19,11 @@ class CharacterRecord extends Equatable {
     required this.strength,
     required this.dexterity,
     required this.intelligence,
+    this.health,
+    this.fatigue,
+    this.coins,
+    this.experience_points,
+    this.attribute_points,
   });
 
   CharacterRecord.fromJson(Map<String, dynamic> json)
@@ -21,7 +31,12 @@ class CharacterRecord extends Equatable {
         name = json['name'],
         strength = json['strength'],
         dexterity = json['dexterity'],
-        intelligence = json['intelligence'];
+        intelligence = json['intelligence'],
+        health = json['health'],
+        fatigue = json['fatigue'],
+        coins = json['coins'],
+        experience_points = json['experience_points'],
+        attribute_points = json['attribute_points'];
 
   Map<String, dynamic> toJson() => {
         'id': id,
@@ -29,6 +44,11 @@ class CharacterRecord extends Equatable {
         'strength': strength,
         'dexterity': dexterity,
         'intelligence': intelligence,
+        'health': health,
+        'fatigue': fatigue,
+        'coins': coins,
+        'experience_points': experience_points,
+        'attribute_points': attribute_points,
       };
 
   @override
