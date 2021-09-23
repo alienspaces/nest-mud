9a4297596ac797d90ee56d6f1d4cfe718d70152d
alienspaces <alienspaces@gmail.com>
2021-09-24 06:34:03 +1000

    Remove unneeded widget

--- client/lib/widgets/home/home_container.dart
+++ client/lib/widgets/home/home_container.dart
@@ -1,8 +1,11 @@
+import 'package:client/widgets/home/home_dungeon.dart';
+import 'package:client/widgets/home/home_character.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 // Application packages
 import 'package:client/logger.dart';
-import 'package:client/widgets/home/home_dungeon_list.dart';
+import 'package:client/cubit/dungeon/dungeon_cubit.dart';
 
 class HomeContainerWidget extends StatefulWidget {
   const HomeContainerWidget({Key? key}) : super(key: key);
@@ -13,10 +16,67 @@ class HomeContainerWidget extends StatefulWidget {
 
 class _HomeContainerWidgetState extends State<HomeContainerWidget> {
   @override
+  void initState() {
+    final log = getLogger('HomeContainerWidget');
+    log.info('Initialising state..');
+
+    super.initState();
+
+    _loadDungeons(context);
+  }
+
+  /// Loads available dungeons
+  void _loadDungeons(BuildContext context) {
+    final log = getLogger('HomeContainerWidget');
+    log.info('Loading dungeons');
+
+    final dungeonCubit = BlocProvider.of<DungeonCubit>(context);
+    dungeonCubit.loadDungeons();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final log = getLogger('HomeContainer');
+    final log = getLogger('HomeContainerWidget');
     log.info('Building..');
 
-    return HomeDungeonListWidget();
+    return BlocConsumer<DungeonCubit, DungeonState>(
+      listener: (context, state) {
+        log.info('listener...');
+      },
+      builder: (BuildContext context, DungeonState state) {
+        log.info('builder...');
+        List<Widget> widgets = [];
+
+        if (state is DungeonStateLoaded) {
+          state.dungeonRecords?.forEach((dungeonRecord) {
+            log.info('Checking dungeon ID ${dungeonRecord.id}');
+            // Dungeon selected
+            if (state is DungeonStateLoaded && state.currentDungeonRecord?.id == dungeonRecord.id) {
+              log.info('Displaying character widget');
+              widgets.add(
+                Container(
+                  child: HomeCharacterWidget(dungeonRecord: dungeonRecord),
+                ),
+              );
+              return;
+            }
+
+            // Dungeon not selected
+            log.info('Displaying dungeon widget');
+            widgets.add(
+              Container(
+                child: HomeDungeonWidget(dungeonRecord: dungeonRecord),
+              ),
+            );
+          });
+        }
+
+        return Container(
+          child: Column(
+            children: widgets,
+          ),
+        );
+      },
+    );
   }
 }
